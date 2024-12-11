944be9ccb9a577ce54fa4de5c2756bb8471bc5f4
tgw-3a <tagawayuu@gmail.com>
2024-12-11 15:20:48 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,9 @@ class Message < ApplicationRecord
   belongs_to :room
   has_one_attached :image
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_image_attached?
+
+  def was_image_attached?
+    self.image.attached?
+  end
 end
